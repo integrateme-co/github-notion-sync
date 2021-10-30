d84fcb76e38cab288490ecfa8d013d847acf8aa0
uzair-ali10 <the.uzairali10@gmail.com>
2021-10-30 17:51:09 +0530

    Make keys Optiional

--- api_data/models.py
+++ api_data/models.py
@@ -6,9 +6,9 @@ from django.db.models.base import Model
 # Create your models here.
 class apiStoreModel(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
-    dev_api = models.CharField(max_length=550)
-    medium_api = models.CharField(max_length=550)
-    hashnode_api = models.CharField(max_length=550)
+    dev_api = models.CharField(max_length=550, blank=True, null=True)
+    medium_api = models.CharField(max_length=550, blank=True, null=True)
+    hashnode_api = models.CharField(max_length=550, blank=True, null=True)
 
 
 class integrationModel(models.Model):

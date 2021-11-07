c8f118514553122ba90a5c6d631cf7a634ee54d8
uzair-ali10 <the.uzairali10@gmail.com>
2021-11-07 10:32:44 +0530

    Added Names to URLS

--- api_data/urls.py
+++ api_data/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . views import get_url, save_apis, save_integration, get_token
 
 urlpatterns = [
-    path('keys', save_apis),
-    path('integration', save_integration),
-    path('url', get_url),
-    path('token', get_token)
+    path('keys', save_apis, name='save_apis'),
+    path('integration', save_integration, name='save_integration'),
+    path('url', get_url, name='get_url'),
+    path('token', get_token, name='get_token'),
 ]
